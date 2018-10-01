e4261038376f7d51fa61657f07a0d4da9806f6f4
Julien Ferrier <jferrier@localhost.localdomain>
2018-10-02 01:31:41 +0200

    maj

---
+++ push_that.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+blih -u julien.ferrier@epitech.eu repository create $1
+blih -u julien.ferrier@epitech.eu repository setacl $1 ramassage-tek r
+blih -u julien.ferrier@epitech.eu repository getacl $1
